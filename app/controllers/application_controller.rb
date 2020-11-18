90d0b306eb6da466f17be064b490111cec8b2b1e
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-18 19:02:19 +0900

    バリデーションの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   #basic認証用コード
   #before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+end
 
-  #private
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
 
   #basic認証用コード
   #def basic_auth
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+         has_many :orders
+         has_many :products
+
+         validates :nickname, presence: true
+         validates :family_name, presence: true
+         validates :first_name, presence: true
+         validates :family_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birthday, presence: true
+
+        end
