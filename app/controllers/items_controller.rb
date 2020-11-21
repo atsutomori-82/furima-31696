b1bfe26857ef05fc171657e6714e2a9867477474
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-21 14:41:43 +0900

    ストロングパラメーターの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :shpping_id, :region_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
