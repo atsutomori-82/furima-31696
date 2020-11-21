58d0defba3ca924b2dbf8ad8c395454278ef372b
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-21 14:31:50 +0900

    active storageの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,9 +6,20 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+        redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new
     @item = Item.new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+  with_options presence: true do
+   validates :name
+   validates :text
+   validates :category_id
+   validates :condition_id
+   validates :shipping_id
+   validates :region_id
+   validates :shipping_day_id
+   validates :price
+  end
 end
