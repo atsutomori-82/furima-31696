2a117d30d3fbb5b79ae9bb0aef95f3b95cf9974c
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-23 14:47:20 +0900

    activehashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+           
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :region, :shipping, :shipping_day
+  validates :category, numericality: { other_than: 1 }
+  validates :condition, numericality: { other_than: 1 }
+  validates :shipping, numericality: { other_than: 1 }
+  validates :region, numericality: { other_than: 1 }
+  validates :shipping_day, numericality: { other_than: 1 }
 
   with_options presence: true do
    validates :name
@@ -13,5 +21,6 @@ class Item < ApplicationRecord
    validates :shipping_day_id
   end
 
-   validates :price, format: { with: \d{4,10} } do
+   validates :price, format: { with: /\d{4,7}/ } do
   end
+end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  {test}
+    text                 {test}
+    category_id              {}
+    condition_id {}
+    region_id {}
+    shipping_day_id {}
+    price {}
+        
   end
 end
