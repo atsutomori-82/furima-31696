f8894f8336edb8084b283b3b127282ad9f3bc13f
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-23 19:04:17 +0900

    正数の指定

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -4,9 +4,9 @@ window.addEventListener('load', () => {
     const inputValue = priceInput.value;
 
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = (inputValue * 0.1);
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
 
     const addProfitDom = document.getElementById("profit");
-    addProfitDom.innerHTML = (inputValue * 0.9);
+    addProfitDom.innerHTML = Math.floor(inputValue * 0.9);
  });
 });
