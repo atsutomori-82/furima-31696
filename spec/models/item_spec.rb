9e67bb9f68bfb210cd02e2c88f897b6500ff22d0
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-25 20:15:05 +0900

    viewファイルの記述修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
- 
+  
+  context '商品出品ができるとき' do
     it "image,name,text,category,condition,shipping,region,shipping_day,priceが存在すれば登録できる" do
       expect(@item).to be_valid
     end
+  end
 
+  context '商品出品ができないとき' do 
   it "商品名が必須であること" do
     @item.name = ''
     @item.valid?  
@@ -121,9 +124,6 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include("Image can't be blank")
   end
   
+  end
+ end
 end
-end
-
-
-"ログアウト状態のユーザーは、商品出品ページへ遷移しようとすると、ログインページへ遷移すること"
-"入力に問題がある状態で出品ボタンが押されたら、出品ページに戻りエラーメッセージが表示されること"
