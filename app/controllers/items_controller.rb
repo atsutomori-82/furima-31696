d5af1dd6f17a90fa3de00482142dfa40f2c873a8
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-25 19:29:23 +0900

    コントローラーの記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
  #before_action :authenticate_user!
- before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def index
     #@items = Item.all.order("created_at DESC")
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :shipping_id, :region_id, :shipping_day_id, :price, :uer ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :shipping_id, :region_id, :shipping_day_id, :price ).merge(user_id: current_user.id)
   end
 
 end
