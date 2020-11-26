b569ed9788c582e683ece393eb9b06c543a16e79
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-26 12:47:51 +0900

    商品がない場合のダミー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
 
 
   def index
-    #@items = Item.all.order("created_at DESC")
+    @items = Item.all.includes(:user, "created_at DESC")
   end
 
   def create
@@ -13,6 +13,9 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
   end
 
   def new
