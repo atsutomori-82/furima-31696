d9f1b21dae8cf7da76c02106bd0d5e1e62289ac0
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-29 21:42:25 +0900

    ストロングパラメーター記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
- before_action :authenticate_user!, :except=>[:index ]
- before_action :set_item
+  before_action :authenticate_user!, :except=>[:index ]
+  before_action :set_item, :only=>[:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -23,12 +23,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
-      unless @item.user_id == current_user.id
+    unless @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
 
   def update
+
       if @item.update(item_params)
         redirect_to item_path(@item.id)
       else
@@ -40,8 +41,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :shipping_id, :region_id, :shipping_day_id, :price ).merge(user_id: current_user.id)
   end
-
+  
   def set_item
     @item = Item.find(params[:id])
   end
+
 end
