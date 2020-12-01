164e39795e31c87c0e3bbf5c0e7b9bdd0bb0e034
atsutomori-82 <atsutomori.82@gmail.com>
2020-12-01 11:47:05 +0900

    バリデーションの修正

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -9,12 +9,14 @@ with_options presence: true do
   validates :shipping_id
   validates :region_id
   validates :shipping_day_id
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
   validates :region_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :tel, format: {with: /\A[0-9]{,11}\z/, message: "is invalid. Only integer"}
 end
 
 def save
   Address.create(post_number: post_number, region_id: region_id, city: city, address: address, building: building, tel: tel)
+end
 
 end
