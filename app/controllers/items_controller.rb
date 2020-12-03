28382c96244b76e269d23a85a13673e02ff1183b
atsutomori-82 <atsutomori.82@gmail.com>
2020-12-03 14:52:04 +0900

    出品者は自分の出品した商品を購入できない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index ]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_root
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -53,6 +54,12 @@ class ItemsController < ApplicationController
   
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_root
+    if current_user.id == @item.user_id || @item.order
+      redirect_to root_path
+    end
   end
 
 end
