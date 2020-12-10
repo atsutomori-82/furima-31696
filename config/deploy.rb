3174273ebcfbe4f57cce0c8eb241450b39558318
atsutomori-82 <atsutomori.82@gmail.com>
2020-12-10 14:47:11 +0900

    deployの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "~> 3.1"
 # Capistranoのログの表示に利用する
 set :application, 'furima-31696'
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, 'git@github.com/atsutomori-82/furima-31696.git'
+set :repo_url, 'git@github.com:atsutomori-82/furima-31696.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
